ffb5aaca4aafa065bdd78e4e80dd1685bda3e008
Maksim Kl <GuruHacker@yandex.ru>
2023-05-27 20:14:55 +0300

    Initial commit

---
+++ HomeWork-4.05.2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "table.h"
+
+int main()
+{
+    auto test = table<int>(2, 3);
+    auto test2 = table<int>(5, 8);
+    test2[0][0] = 234;
+    std::cout << "Size table2: " << test2.Size() << std::endl;
+    std::cout << "table2[0][0]: " << test2[0][0] << std::endl;;
+    test[0][0] = 4;
+    test2 = test;
+    std::cout << "Size table2 after '=': " << test2.Size() << std::endl;
+    std::cout << "table2[0][0]: " << test2[0][0];
+}
---
+++ table.h
@@ -0,0 +1,70 @@
+#pragma once
+template <class T>
+class table
+{
+public:
+	table(int row, int col);
+	~table();
+	table& operator=(const table& t);
+	T* operator[](int row);
+	T* operator[](int row) const;
+	int Size() const;
+private:
+	int col_;
+	int row_;
+	T** data_;
+};
+
+template <class T>
+table<T>::table(int row, int col) : row_(row), col_(col) {
+	if (row_ < 0 || col_ < 0) throw "Размер таблицы меньше 0";
+	data_ = new T * [row_];
+	for (int i = 0; i < row_; ++i) {
+		data_[i] = new T[col_];
+	}
+};
+
+template <class T>
+table<T>::~table() {
+	for (int i = 0; i < row_; ++i) {
+		delete[] data_[i];
+		data_[i] = nullptr;
+	}
+	delete data_;
+	data_ = nullptr;
+};
+
+template <class T>
+T* table<T>::operator[](int row) {
+	if (row < 0 || row > row_) throw "Индекс вне диапозона";
+	return data_[row];
+}
+
+template <class T>
+T* table<T>::operator[](int row) const {
+	if (row < 0 || row > row_) throw "Индекс вне диапозона";
+	return data_[row];
+}
+
+template <class T>
+table<T>& table<T>::operator=(const table& rt) {
+	for (int i = 0; i < row_; ++i) {
+		delete[] data_[i];
+	}
+	delete data_;
+	row_ = rt.row_;
+	col_ = rt.col_;
+	data_ = new T * [row_];
+	for (int i = 0; i < row_; ++i) {
+		data_[i] = new T[col_];
+		for (int j = 0; j < col_; ++j) {
+			data_[i][j] = rt.data_[i][j];
+		}
+	}
+	return *this;
+}
+
+template <class T>
+int table<T>::Size() const {
+	return row_ * col_;
+}
